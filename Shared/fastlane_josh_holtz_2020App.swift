90c94fbed076c9b49aa80b20de8898a83ba44503
Alexander Zou <alexander.zou@rallyhealth.com>
2021-03-18 19:20:07 -0700

    Initial Commit

---
+++ Shared/fastlane_josh_holtz_2020App.swift
@@ -0,0 +1,17 @@
+//
+//  fastlane_josh_holtz_2020App.swift
+//  Shared
+//
+//  Created by Alexander Zou on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct fastlane_josh_holtz_2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
